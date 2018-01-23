bd535663b03797ba7cc34a51f9db6b8462159181
Steve <acer_10@yahoo.com>
2018-01-23 15:11:57 -0500

    Update milestones.js

--- js/milestones.js
+++ js/milestones.js
@@ -42,6 +42,7 @@
 			/*-----------------------------
 			Insert the Milestone components
 			------------------------------*/
+			$(this).AddClass('milestones');
 			$(this).html('<div class="progress"><div class="progress-bar" role="progressbar" style="width:'+len+'%"> </div></div><div class="stage"></div><div class="labels"></div><div class="alt-label">Next Action: <span>'+labels[stage]+'</span></div>');
 
 			/*-----------------------------
