3c194ca10965e26917deed33a01b3d0207adb3c6
stemo40 <acer_10@yahoo.com>
2018-01-29 22:05:54 -0500

    updated

--- docs/conf.py
+++ docs/conf.py
@@ -138,7 +138,7 @@ latex_elements = {
 
     # The font size ('10pt', '11pt' or '12pt').
     #
-     'pointsize': '12pt',
+     'pointsize': '11pt',
 
     # Additional stuff for the LaTeX preamble.
     #
